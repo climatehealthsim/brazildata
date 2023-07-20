fd37a1e6195c829692219e6e2f0df612f01c2601
Christian Jaeger <ch@christianjaeger.ch>
2023-07-20 05:10:46 +0200

    recent8: cleanup: consistency

--- src/recent8.rs
+++ src/recent8.rs
@@ -4,6 +4,28 @@ use serde::Deserialize;
 
 use crate::{easycsv::{self, CsvOption}, util::{remove_coordinates, CapitalDeNotificacao, RecentTable}};
 
+#[derive(Debug, Deserialize)]
+#[allow(dead_code)]
+pub struct RecentRecord8 {
+    coordinates_and_capital_de_notificacao: KString,
+    pub ign_per_white: CsvOption<u64>,
+    pub illiterate: CsvOption<u64>,
+    pub _1st_to_4th_incomplete_grade_of_fs: CsvOption<u64>,
+    pub _4th_complete_grade_of_fs: CsvOption<u64>,
+    pub _5th_to_8th_incomplete_grade_of_fs: CsvOption<u64>,
+    pub complete_elementary_school: CsvOption<u64>,
+    pub incomplete_medium_education: CsvOption<u64>,
+    pub complete_medium_education: CsvOption<u64>,
+    pub incomplete_higher_education: CsvOption<u64>,
+    pub complete_higher_education: CsvOption<u64>,
+    pub does_not_apply: CsvOption<u64>,
+}
+
+impl CapitalDeNotificacao for RecentRecord8 {
+    fn capital_de_notificacao(&self) -> Result<&str> {
+        remove_coordinates(&self.coordinates_and_capital_de_notificacao)
+    }
+}
 
 // Notification capital	Ign/White	Illiterate	1st to 4th incomplete grade of FS	4th complete grade of FS	5th to 8th incomplete grade of FS	Complete elementary school	Incomplete high school	Complete higher education	Incomplete higher education	Complete higher education	Does not apply
 const TSV : &'static str = "Capital de notificação	Ign/Branco	Analfabeto	1ª a 4ª série incompleta do EF	4ª série completa do EF	5ª a 8ª série incompleta do EF	Ensino fundamental completo	Ensino médio incompleto	Ensino médio completo	Educação superior incompleta	Educação superior completa	Não se aplica
@@ -36,28 +58,6 @@ const TSV : &'static str = "Capital de notificação	Ign/Branco	Analfabeto	1ª a
 510340 Cuiabá	-	-	-	2	-	2	1	1	-	-	-
 ";
 
-#[derive(Debug, Deserialize)]
-#[allow(dead_code)]
-pub struct RecentRecord8 {
-    coordinates_and_capital_de_notificacao: KString,
-    pub ign_per_white: CsvOption<u64>,
-    pub illiterate: CsvOption<u64>,
-    pub _1st_to_4th_incomplete_grade_of_fs: CsvOption<u64>,
-    pub _4th_complete_grade_of_fs: CsvOption<u64>,
-    pub _5th_to_8th_incomplete_grade_of_fs: CsvOption<u64>,
-    pub complete_elementary_school: CsvOption<u64>,
-    pub incomplete_medium_education: CsvOption<u64>,
-    pub complete_medium_education: CsvOption<u64>,
-    pub incomplete_higher_education: CsvOption<u64>,
-    pub complete_higher_education: CsvOption<u64>,
-    pub does_not_apply: CsvOption<u64>,
-}
-
-impl CapitalDeNotificacao for RecentRecord8 {
-    fn capital_de_notificacao(&self) -> Result<&str> {
-        remove_coordinates(&self.coordinates_and_capital_de_notificacao)
-    }
-}
 
 pub struct RecentTable8 {
     desc: String,
